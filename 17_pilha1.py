daa21ea11463255ee08c9f1c7e41e6dffa4a4986
Kauê José <jkaue986@gmail.com>
2023-11-06 21:06:48 -0300

    (06/11)

--- 17_pilha1.py
+++ 17_pilha1.py
@@ -9,3 +9,18 @@ palavra = "LARANJA"
 for letra in palavra:
     pilha.push(letra)
     print(pilha)
+
+print( '-' * 80 )
+
+inverso = ""
+
+# Desmepilhando e colocando cada letra retirada
+# dentro da string inverso
+while not pilha.is_empty():
+    inverso += pilha.pop()
+    print(pilha)
+
+print( '-' * 80 )
+
+print(f"Palavra original: {palavra}")
+print(f"Palavra invertida: {inverso}")
